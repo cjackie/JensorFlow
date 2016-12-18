8ea9c9b67719963fef8c26f5242e3b5267793c1f
chaojie <chaojie.keepgoing@gmail.com>
2016-12-18 13:16:44 -0500

    fix a bug

--- proto/LinearLayer.py
+++ proto/LinearLayer.py
@@ -37,7 +37,7 @@ class LinearLayer():
         assert lossDerivUpperLayer.shape == (self._nOutputs, 1)
         return self._thetas * lossDerivUpperLayer
 
-    @accept(LinearLayer, matrix)
+    @accept(LinearLayer, matrix, matrix)
     def computeParamGradients(self, lossDerivUpperLayer, inputVector):
         '''
         compute the gradient wrt the loss of each parameter.
@@ -53,15 +53,44 @@ class LinearLayer():
         return a vector of parameters.
         @return :matrix of shape (self._nInputs*self._nOutputs, 1). 
         '''
-        return self._thetas.reshape(self._nInputs*self._nOutputs)
+        return self._thetas.reshape(self._nInputs*self._nOutputs, 1)
 
-    @accept(LinearLayer, ndarray)
+    @accept(LinearLayer, matrix)
     def setParams(self, thetas):
         '''
         set the parameters.
-        @thetas :ndarray. 
+        @thetas :matrix of shape (self._nInputs*self._nOutputs, 1). 
         '''
-        assert thetas.size == self._nIutputs*self._nOutputs
-        self._thetas = matrix(thetas, copy=True)
+        assert thetas.shape == (self._nIutputs*self._nOutputs, 1)
+        self._thetas = thetas.copy()
         self._thetas.reshape(self._nInputs, self.nOutputs)
+
+
+if '__main__' == __name__:
+    layer = LinearLayer(3, 2)
+    # parameters
+    parameters = layer.getParams()
+    message = "Parameters is " + str(parameters)
+    print(message)
+    # forward pass
+    inputs = matrix([2,3,4])
+    forwardValues = layer.forward(inputs)
+    message = "With forward inputs " + str(inputs) + \
+        ", forward values is " + str(forwardValues)
+    print(message)
+    # backprop
+    accumulativeErrorVector = matrix([[2], [3]])
+    backpropValues = layer.backprop(accumulativeErrorVector)
+    message = "Backprop output is " + str(backpropValues)
+    print(message)
+    # get gradients
+    gradients = gradients(accumulativeErrorVector, inputs)
+    message = "Gradient is " + str(gradients)
+    print(message)
+
+
+
+
+
+
 
